15f5eb92b5f52c8476c51906e4eb4797402b7695
aluochvivian <aluochviviann@gmail.com>
2023-05-24 10:01:45 +0300

    Update

--- script.js
+++ script.js
@@ -1,143 +1,146 @@
 const selectors = {
-    boardContainer : document.querySelector('.board-container'),
-    board : document.querySelector('.board'),
-    moves : document.querySelector('moves'),
-    timer : document.querySelector('timer'),
-    start : document.querySelector('button'),
-    win : document.querySelector('win')
-};
+    boardContainer: document.querySelector('.board-container'),
+    board: document.querySelector('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
 
 const state = {
-    gameStarted : false,
-    flippedCards : 0,
-    totalFlips : 0,
-    totalTime : 0,
-    loop : null
-};
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
+
 const shuffle = array => {
-    const clonedArray = [...array];
+    const clonedArray = [...array]
 
-    for(let i = clonedArray.length -1; i > 0; i--){
-        const randomIndex = Math.floor(Math.random() * (i + 1));
-        const original = clonedArray[i];
+    for (let i = clonedArray.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1))
+        const original = clonedArray[i]
 
-        clonedArray[i] = clonedArray[randomIndex];
-        clonedArray[randomIndex] = origin;
+        clonedArray[i] = clonedArray[randomIndex]
+        clonedArray[randomIndex] = original
     }
-    return clonedArray;
+
+    return clonedArray
 }
 
 const pickRandom = (array, items) => {
-    const clonedArray = [...array];
-    const randomPicks = [];
-    for(let i=0; i<items; i++){
-    const randomIndex = Math.floor(Math.random() * clonedArray.length);
+    const clonedArray = [...array]
+    const randomPicks = []
 
-    randomPicks.push(clonedArray[randomIndex]);
-    clonedArray.splice(randomIndex, 1);
-}
-return randomPicks;
+    for (let i = 0; i < items; i++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+        
+        randomPicks.push(clonedArray[randomIndex])
+        clonedArray.splice(randomIndex, 1)
+    }
+
+    return randomPicks
 }
 
 const generateGame = () => {
-    const dimensions = selectors.board.getAttribute('data-dimensio');
+    const dimensions = selectors.board.getAttribute('data-dimension')  
 
-    if(dimension % 2 !== 0){
+    if (dimensions % 2 !== 0) {
         throw new Error("The dimension of the board must be an even number.")
     }
-    const emojis = ['🍎','🍌','🍇', '🍊','🍓','🍉','🍍','🍒','🍑','🥥']
-    const picks = pickRandom(emojis, (dimensions * dimensions) /2);
-    const items = shuffle ([...picks, ...picks]);
-    const cards = `
-      <div class="board" style="grid-template-columns: repeat(${dimensions},
-        auto)">
-        ${items.map(item => `
-            <div class="card">
-                 <div class="card-front"></div>
-                 <div class="card-back">${item}</div>
-        `).join('')}
-        </div>
-    `
 
-    const parser = new DOMParser().parseFromString(cards, 'text/html');
+    const emojis = ['🍎', '🍌', '🍇', '🍊', '🍓', '🍉', '🍍', '🍒', '🍑', '🥥']
+    const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
+    const items = shuffle([...picks, ...picks])
+    const cards = `
+        <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
+            ${items.map(item => `
+                <div class="card">
+                    <div class="card-front"></div>
+                    <div class="card-back">${item}</div>
+                </div>
+            `).join('')}
+       </div>
+    `
+    
+    const parser = new DOMParser().parseFromString(cards, 'text/html')
+
     selectors.board.replaceWith(parser.querySelector('.board'))
-};
+}
 
 const startGame = () => {
-    state.gameStarted = true;
-    selectors.start.classList.add('disabled');
+    state.gameStarted = true
+    selectors.start.classList.add('disabled')
 
     state.loop = setInterval(() => {
-        state.totalTime++;
+        state.totalTime++
 
-        selectors.moves.innerText = `${state.totalFlips} moves`;
+        selectors.moves.innerText = `${state.totalFlips} moves`
         selectors.timer.innerText = `Time: ${state.totalTime} sec`
-}, 1000)
-
+    }, 1000)
 }
-const flipBackcards = () => {
-    document.querySelectorAll('.card:not(matched)').forEach(card => {
-        card.classList.remove('.flipped');
-        
-    });
-    state.flippedCards = 0;
+
+const flipBackCards = () => {
+    document.querySelectorAll('.card:not(.matched)').forEach(card => {
+        card.classList.remove('flipped')
+    })
+
+    state.flippedCards = 0
 }
 
 const flipCard = card => {
-    state.flippedCards++;
-    state.totalFlips++;
+    state.flippedCards++
+    state.totalFlips++
 
-    if (!state.gameStarted){
+    if (!state.gameStarted) {
         startGame()
     }
-    if(state.flippedCards <= 2){
+
+    if (state.flippedCards <= 2) {
         card.classList.add('flipped')
     }
-    if(state.flippedCards === 2){
-        const flippedCards = document.querySelectorAll('.flipped:not(.matched)');
-        if(flippedCards[0].innerText === flippedCards[1].innerText){
-            flippedCards[0].classList.add('.matched');
-            flippedCards[1].classList.add('.matched');
+
+    if (state.flippedCards === 2) {
+        const flippedCards = document.querySelectorAll('.flipped:not(.matched)')
+
+        if (flippedCards[0].innerText === flippedCards[1].innerText) {
+            flippedCards[0].classList.add('matched')
+            flippedCards[1].classList.add('matched')
         }
-        
+
         setTimeout(() => {
-               flipBackcards()
-    }, 1000)
-
+            flipBackCards()
+        }, 1000)
     }
+    if (!document.querySelectorAll('.card:not(.flipped)').length) {
+        setTimeout(() => {
+            selectors.boardContainer.classList.add('flipped')
+            selectors.win.innerHTML = `
+                <span class="win-text">
+                    You won!<br />
+                    with <span class="highlight">${state.totalFlips}</span> moves<br />
+                    under <span class="highlight">${state.totalTime}</span> seconds
+                </span>
+            `
 
-if(!document.querySelectorAll('.card:not(flipped)').length) {
-    setTimeout(() => {
-         selectors.boardContainer.classList.add('flipped')
-         selectors.win.innerHTML = `
-             <span class="win-text">
-             You won!<br />
-             with <span class="highlight">${state.totalFlips}</span>
-             moves<br />
-             under <span class="highlight">${state.totalTime}</span>
-             seconds
-        </span>
-
-         `
-
-         clearInterval(satte.loop)
-   }, 1000)
- }
+            clearInterval(state.loop)
+        }, 1000)
+    }
 }
 
 const attachEventListeners = () => {
-    document.addEventListener('click',event => {
-         const eventTarget = event.target;
-         const eventParent = eventTarget.parentElement;
-        
-         if(eventTarget.className.includes('cards') && !eventParent.className.
-         includes('flipped')){
-            flipCard(eventParent)
-         }else if(eventTarget.nodeName === 'BUTTON' && !eventTarget.className.
-         includes('disabled')){
-            startGame();
-         }
+    document.addEventListener('click', event => {
+        const eventTarget = event.target
+        const eventParent = eventTarget.parentElement
 
-        })
-    
+        if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
+            flipCard(eventParent)
+        } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
+            startGame()
+        }
+    })
 }
+
+generateGame()
+attachEventListeners()
